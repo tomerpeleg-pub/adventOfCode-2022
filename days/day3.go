b124c9267c865cff4d41161958035ada43f6712d
Tomer Peleg <tomer.peleg@gmail.com>
2022-12-10 15:25:20 +0000

    Day 3 P2

--- days/day3.go
+++ days/day3.go
@@ -5,43 +5,80 @@ import (
 	"strings"
 )
 
+type rucksack = map[rune]bool
+
+func parseRucksack(rucksackStr string) rucksack {
+	items := rucksack{}
+
+	for _, item := range rucksackStr {
+		items[item] = true
+	}
+
+	return items
+}
+
+func findMatching(rucksacks []rucksack) rune {
+
+outside:
+	for key := range rucksacks[0] {
+		for _, rucksack := range rucksacks {
+			if !rucksack[key] {
+				continue outside
+			}
+		}
+
+		return key
+	}
+
+	return 'a'
+}
+
+func getItemScore(item rune) int {
+	if item <= 'Z' {
+		return 27 + (int(item) - 'A')
+	} else {
+		return 1 + (int(item) - 'a')
+	}
+}
+
 func Day3Part1(input string) int {
 	lines := strings.Split(input, "\n")
 	score := 0
 
 	// Loop through each rucksack
 	for _, line := range lines {
-		items := map[rune]int{}
 		lineLen := len(line) / 2
-
-		// Loop through each item in the rucksack
-		for i, char := range line {
-
-			if i < lineLen {
-				// First compartment
-				// Store each type of item seen (don't care how many)
-				items[char] = 1
-			} else if items[char] == 1 {
-				// Second compartment, only care about items already
-				//  seen in the first compartment
-				// Mark item as seen in both
-				items[char] = 2
-
-				// Add score depending on type of item
-				if char <= 'Z' {
-					score += 27 + (int(char) - 'A')
-				} else {
-					score += 1 + (int(char) - 'a')
-				}
-			}
+		rucksacks := []rucksack{
+			parseRucksack(line[0:lineLen]),
+			parseRucksack(line[lineLen:]),
 		}
+		matchingItem := findMatching(rucksacks)
+		score += getItemScore(matchingItem)
+	}
+
+	return score
+}
+
+func Day3Part2(input string) int {
+	lines := strings.Split(input, "\n")
+	score := 0
+
+	// Loop through each rucksack
+	for i := 0; i < len(lines)-2; i += 3 {
+		rucksacks := []rucksack{
+			parseRucksack(lines[i]),
+			parseRucksack(lines[i+1]),
+			parseRucksack(lines[i+2]),
+		}
+		matchingItem := findMatching(rucksacks)
+		score += getItemScore(matchingItem)
 	}
 
 	return score
 }
 
 func Day3(input string) {
-	fmt.Println("Day 2 -----")
+	fmt.Println("Day 3 -----")
 	fmt.Println("Part 1:", Day3Part1(input))
-	// fmt.Println("Part 2:", Day2Part2(input))
+	fmt.Println("Part 2:", Day3Part2(input))
 }
--- days/day3_test.go
+++ days/day3_test.go
@@ -19,3 +19,14 @@ func TestDay3Part1(t *testing.T) {
 		t.Errorf("Got %v but wanted %v", got, want)
 	}
 }
+
+func TestDay3Part2(t *testing.T) {
+	t.Log("Testing Day3 Part 2")
+
+	want := 70
+	got := Day3Part2(example3)
+
+	if got != want {
+		t.Errorf("Got %v but wanted %v", got, want)
+	}
+}
